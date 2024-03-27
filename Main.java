5d3132ed0b29e6a939091693c26c3801de008721
vvlad-islavs <104062267+vvlad-islavs@users.noreply.github.com>
2024-03-28 01:28:17 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -6,77 +8,98 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     protected static int index;
     protected static int oper;
-    protected static int arabian;
-    protected static int arabian2;
+    protected static int roman;
+    protected static int roman2;
     protected static boolean isRoman;
 
 
-    public static void main(String[] args) {
-        HashMap<Integer, String> maparab = new HashMap<>();
-        maparab.put(1, "I");
-        maparab.put(2, "II");
-        maparab.put(3, "III");
-        maparab.put(4, "IV");
-        maparab.put(5, "V");
-        maparab.put(6, "VI");
-        maparab.put(7, "VII");
-        maparab.put(8, "VIII");
-        maparab.put(9, "IX");
-        maparab.put(10, "X");
-        maparab.put(50, "L");
-        maparab.put(100, "C");
-        maparab.put(500, "D");
-        maparab.put(1000, "M");
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer, String> mapRoman = new HashMap<>();
+        mapRoman.put(1, "I");
+        mapRoman.put(2, "II");
+        mapRoman.put(3, "III");
+        mapRoman.put(4, "IV");
+        mapRoman.put(5, "V");
+        mapRoman.put(6, "VI");
+        mapRoman.put(7, "VII");
+        mapRoman.put(8, "VIII");
+        mapRoman.put(9, "IX");
+        mapRoman.put(10, "X");
+//        mapRoman.put(50, "L");
+//        mapRoman.put(100, "C");
+//        mapRoman.put(500, "D");
+//        mapRoman.put(1000, "M");
 
 
-        System.out.println("Напишите выржаение:");
+        System.out.println("Напишите выражение:");
         String str;
-        str = getStroke(maparab);
+        str = getStroke(mapRoman);
         if (!(str == null)) {
             System.out.println(str);
         }
     }
 
-    private static String getStroke(HashMap maparab) {
+    private static String getStroke (HashMap mapRoman) throws IOException {
         String stroke = scanner.nextLine().trim();
-        index = stroke.indexOf("+");
+//        index = stroke.indexOf("+");
         if (stroke.contains("+") ||
                 stroke.contains("*") ||
                 stroke.contains("-") ||
                 stroke.contains("/")) {
-            getOperation(stroke, maparab);
+            getOperation(stroke, mapRoman);
             return null;
-        } else
-            return "Неверное выражение!";
+        } else {
+//            return "Неверное выражение!";
+            throw new IOException();
+        }
     }
+
 
     private static void getOperation(String stroke, HashMap mapRoman) {
         if (stroke.contains("+")) {
             oper = '+';
+//            index = stroke.indexOf('+');
         }
         if (stroke.contains("*")) {
-
             oper = '*';
+//            index = stroke.indexOf('*');
         }
         if (stroke.contains("-")) {
             oper = '-';
+//            index = stroke.indexOf('-');
         }
         if (stroke.contains("/")) {
             oper = '/';
+//            index = stroke.indexOf('/');
         }
         index = stroke.indexOf(oper);
         isRoman = isFirstRoman(mapRoman, stroke) & isSecondRoman(mapRoman, stroke);
 
         if (isRoman) {
 //            System.out.println("роман");
-            operation(arabian, arabian2);
+            operation(roman, roman2);
 
         } else if (isSecondRoman(mapRoman, stroke) || isFirstRoman(mapRoman, stroke)) {
-            System.out.println("Неверное выражение!");
+//            System.out.println("Неверное выражение!");
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } else {
             int first = Integer.parseInt(stroke.substring(0, index).trim());
             int second = Integer.parseInt(stroke.substring(index + 1, stroke.length()).trim());
-            operation(first, second);
+            if (first<=10&second<=10){
+                operation(first, second);
+            }
+            else {
+                try {
+                    throw new IOException();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+//                System.out.println("Число больше 10!");
+            }
 
         }
     }
@@ -85,7 +108,7 @@ public class Main {
     private static boolean isFirstRoman(HashMap map, String stroke) {
         for (int i = 0; i < 11; i++) {
             if (stroke.substring(0, index).trim().equals(map.get(i))) {
-                arabian = i;
+                roman = i;
                 return true;
             }
         }
@@ -96,12 +119,12 @@ public class Main {
 
         for (int i = 0; i < 11; i++) {
             if (stroke.substring(index + 1, stroke.length()).trim().equals(map.get(i))) {
-                arabian2 = i;
-//                System.out.println(arabian2);
+                roman2 = i;
+//                System.out.println(roman2);
                 return true;
             }
         }
-//        System.out.println(arabian2);
+//        System.out.println(roman2);
         return false;
 
     }
@@ -121,15 +144,29 @@ public class Main {
 //            System.out.println("Умножение");
         }
         if (oper == '/') {
-            res = first / second;
+            if(second != 0)
+                res = first / second;
+            else
+                try {
+                    throw new IOException();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 //            System.out.println("Деление");
         }
-        if (isRoman & res > 0) {
-            converter(res);
-
-        } else if (!isRoman){
+        if (isRoman) {
+            if(res>0)
+                converter(res);
+            else {
+                try {
+                    throw new IOException();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else if(!isRoman)
             System.out.println(res);
-        } else System.out.println("Невероное выражение!");
+
     }
     enum RomanNumeral {
         I(1), IV(4), V(5), IX(9), X(10),
